07549257b7bc9929c86bf74f3bf003f718c0be91
Bhaskar Govil <124559766+bhaskargovil@users.noreply.github.com>
2024-05-12 14:16:30 +0000

    updated AllPosts to MyPosts 1.1

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import service from "../../appwrite/auth_service_doc";
 import Container from "../container/Container";
-import AllPosts from "./AllPosts";
+import MyPosts from './MyPosts'
 import { useSelector } from "react-redux";
 
 function Home() {
@@ -32,7 +32,21 @@ function Home() {
       </>
     );
   } else {
-    return <AllPosts />;
+    return (
+      <div className="py-8 w-full">
+        <Container>
+          <div className="flex flex-wrap">
+            {posts.map((post) => {
+              return (
+                <div key={post.$id} className="p-2 w-1/4">
+                  <Postcard {...post} />
+                </div>
+              );
+            })}
+          </div>
+        </Container>
+      </div>
+    );
   }
 }
 
